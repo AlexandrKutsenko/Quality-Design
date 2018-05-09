70357ce1d4835bb14c7df54244671be0c970ff2f
Alexandr Kutsenko <accauamoro@gmail.com>
2018-05-09 11:14:47 +0200

    end

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
             1000:{
                 items:8,
                 nav:true,
-                loop:false
+                loop:true
             }
         }
     });
